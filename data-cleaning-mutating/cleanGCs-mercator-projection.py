2977ac9558d4837b5e9f22e55116446ae6fe17d3
Ronan Potts <ronanpotts6@gmail.com>
2022-08-19 09:18:12 +1000

    Added cleanGCs-mercator-projection.py
    Maps Dec. and RA data to a mercator projection for 2D visualisation

---
+++ data-cleaning-mutating/cleanGCs-mercator-projection.py
@@ -0,0 +1,52 @@
+'''
+The purpose of this file is to map the GCs onto a 2D surface with which we can visualise the positions of the GCs and
+potentially create a vector field of the velocities of each GC. I am using equations (1) and (6) from 
+https://mathworld.wolfram.com/MercatorProjection.html. Here, right ascension (RA) is the analogue for longitude, while
+declination is the analogue for latitude.
+
+The Mercator projection is used as it keeps lines of latitude and longitude parallel and straight, meaning it may be
+easier to see a broader rotation or movement in the galaxy clusters.
+
+
+Fix for later: try to cluster galaxies within the plt.quiver() as dense points so that the plot looks less messy.
+'''
+
+import pandas as pd
+import numpy as np
+import os
+import matplotlib.pyplot as plt
+
+# Accessing the file
+filePath = 'clean-clusters/'
+fileNames = os.listdir(filePath)
+
+mean_x = []
+mean_y = []
+mean_vx = []
+mean_vy = []
+size = []
+
+for fileName in fileNames:
+    f_data = pd.read_csv(filePath+fileName,header=0)
+    
+    f_data = f_data.drop(['x', 'y', 'vx', 'vy', 'plx', 'memberprob', 'pmcorr', 'plxe'], axis=1)
+
+    longitude = f_data['ra']
+    latitude = f_data['dec']
+
+    f_data['x_mercator'] = longitude - 180
+    f_data['y_mercator'] = np.log(np.tan(np.deg2rad(latitude)) + (1/np.cos(np.deg2rad(latitude))))
+
+    mean_x_mercator = np.mean(f_data['x_mercator'])
+    mean_y_mercator = np.mean(f_data['y_mercator'])
+    mean_vx_mercator = np.mean(f_data['pmra'])
+    mean_vy_mercator = np.mean(f_data['pmdec'])
+
+    mean_x.append(mean_x_mercator)
+    mean_y.append(mean_y_mercator)
+    mean_vx.append(mean_vx_mercator)
+    mean_vy.append(mean_vy_mercator)
+    size.append(len(f_data['ra']))
+
+plt.quiver(mean_x,mean_y,mean_vx,mean_vy,width=0.0025,pivot="mid")
+plt.show()
