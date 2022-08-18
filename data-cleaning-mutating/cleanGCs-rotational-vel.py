d166ba4ed30d8c0696384a94a53d8334daf9163d
Ronan Potts <ronanpotts6@gmail.com>
2022-08-19 09:19:06 +1000

    Added cleanGCs-rotational-vel
    Possible use in calculating rotational velocities of stars in GCs

---
+++ data-cleaning-mutating/cleanGCs-rotational-vel.py
@@ -0,0 +1,22 @@
+import pandas as pd
+import numpy as np
+import os
+
+# Accessing the file
+filePath = 'clean-clusters/'
+fileNames = os.listdir(filePath)
+
+for fileName in fileNames:
+    f_data = pd.read_csv(filePath+fileName)
+
+    f_data = f_data[abs(f_data.plxe) < 0.1*abs(f_data.plx)]
+
+    '''
+    Approximately 0.1% of data has a parallax error of less than 1/10th the parallax.
+    We probably shouldn't look at parallax as a reliable measure of distance, unless we consider sampling and calculate sample errors.
+    Sampling would require a hypothesis for the distance distribution of stars in GCs, which we don't really have (ask Ciaran).
+    '''
+
+    if fileName == "NGC_5139_oCen.txt":
+        print(f_data.plxe)
+        print(len(f_data.plxe))
